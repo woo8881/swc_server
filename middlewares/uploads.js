c153e50dc072fd9bfa634143d2fa61f9b6faec74
sungHoNoteBook <12woo34@naver.com>
2022-03-31 16:27:58 +0900

    init

--- middlewares/uploads.js
+++ middlewares/uploads.js
@@ -14,8 +14,8 @@ const imageFilter = (req, file, cb) => {
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
     // 서버에 저장될 위치
-    cb(null, "/home/hosting_users/bcd1031/apps/bcd1031_swc/images")
-    console.log(path.join("/home/hosting_users/bcd1031/apps/bcd1031_swc/images"));
+    cb(null, "http://swc.cafe24app.com/home/hosting_users/bcd1031/apps/bcd1031_swc/images")
+    console.log(path.join("http://swc.cafe24app.com/home/hosting_users/bcd1031/apps/bcd1031_swc/images"));
   },
 
   filename: (req, file, cb) => {
