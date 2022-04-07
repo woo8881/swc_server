7e281daaf6e446637575be5ae1871ddb769971a4
Sungho Jung <12woo34@naver.com>
2022-04-07 16:52:59 +0900

    init

--- routes/user/userService.js
+++ routes/user/userService.js
@@ -93,7 +93,7 @@ module.exports = {
           result == 1 ? resolve(true) : resolve(false);
         })
         .catch((err) => {
-          throw err;
+          console.log(err);
         });
     });
   },
