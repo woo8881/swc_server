6415e92394a31e52591c25e92477dcf1b196fe25
Sungho Jung <12woo34@naver.com>
2022-04-11 23:44:06 +0900

    init

--- models/user/foreignKey.js
+++ models/user/foreignKey.js
@@ -1,7 +1,8 @@
 module.exports = (db) => {
     db.User.hasMany(db.Board, {
       foreignKey: "user_id",
-      sourceKey: "user_id"
+      sourceKey: "user_id",
+      allowNull: false,
     });
     db.User.hasMany(db.Comment, {
         foreignKey: "user_id",
