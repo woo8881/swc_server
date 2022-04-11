e15e9a7144663e9d012bd7c2acad3900d56df9d6
Sungho Jung <12woo34@naver.com>
2022-04-11 22:44:30 +0900

    init

--- models/board/foreignKey.js
+++ models/board/foreignKey.js
@@ -1,28 +1,22 @@
-module.exports = db => {
-    db.Board.belongsTo(db.User, {
-        foreignKey: "user_id"
-    });
+module.exports = (db) => {
+  db.Board.belongsTo(db.User, {
+    foreignKey: "user_id",
+  });
 
-
-
-
-    db.Board.hasMany(db.Comment, {
-      foreignKey: "board_id",
-      sourceKey: "board_id"
-    });
-    db.Board.hasMany(db.Likes, {
-      foreignKey: "board_id",
-      sourceKey: "board_id"
-    });
-    db.Board.hasMany(db.Team, {
-      foreignKey: "board_id",
-      sourceKey: "board_id"
-    });
-    db.Board.hasMany(db.Photo,{
-      foreignKey:"board_id",
-      sourceKey:"board_id"
-    });
-
-}
-
-
+  db.Board.hasMany(db.Comment, {
+    foreignKey: "board_id",
+    sourceKey: "board_id",
+  });
+  db.Board.hasMany(db.Likes, {
+    foreignKey: "board_id",
+    sourceKey: "board_id",
+  });
+  db.Board.hasMany(db.Team, {
+    foreignKey: "board_id",
+    sourceKey: "board_id",
+  });
+  db.Board.hasMany(db.Photo, {
+    foreignKey: "board_id",
+    sourceKey: "board_id",
+  });
+};
