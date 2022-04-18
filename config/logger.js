eae718fdacdc0d414a068462e685b9b46eb6294f
sungHoNoteBook <12woo34@naver.com>
2022-04-18 14:20:41 +0900

    init

--- config/logger.js
+++ config/logger.js
@@ -1,6 +1,7 @@
 const winston = require('winston');
 require('winston-daily-rotate-file');
-const logDir = 'logs';
+var fs = require('fs');
+const logDir = '${__dirname}/logs';
 
 const levels = {
     error: 0,
