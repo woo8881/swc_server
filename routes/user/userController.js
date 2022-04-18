ee94638e344be81b95dd3b673b237391cbd9cd51
sungHoNoteBook <12woo34@naver.com>
2022-04-18 11:35:48 +0900

    init

--- routes/user/userController.js
+++ routes/user/userController.js
@@ -26,7 +26,7 @@ module.exports = {
         obj["err"] == "메일 보내기 실패"
         res.send(obj);
       } else{
-        console.log(result.html)
+        delete result.html
         obj["suc"] =true;
         obj["email"] = result;
         res.send(obj);
