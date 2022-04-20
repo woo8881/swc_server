d160367b26d57f5d9e1a85690740622705196dcb
Sungho Jung <12woo34@naver.com>
2022-04-20 16:09:29 +0900

    init

--- web.js
+++ web.js
@@ -29,12 +29,21 @@ app.engine("html", ejs.renderFile);
 sequelize.sync();
 app.use('/', router);
 
+app.set('port', process.env.PORT || 8001);
+app.use('/public', static(path.join(__dirname, 'public')));
 
+//==== 서버 실행 ====//
+const httpServer = http.createServer(app).listen(app.get('port'), function(){
+    console.log('서버가 시작되었습니다. 포트 : ', app.get('port'));
+});
+
+//==== socket.io ====//
 const io = require("socket.io")(httpServer);
 console.log('socket.io 요청을 받아들일 준비가 되었습니다.');
 
+// 클라이언트가 연결했을 때의 이벤트 처리
 io.on("connection", (socket) => {
-  console.log('connection info : ', socket.request.connection._peername);
+    console.log('connection info : ', socket.request.connection._peername);
 });
 //파일 있는지 보는거
 
@@ -46,9 +55,12 @@ io.on("connection", (socket) => {
 // )
 // 
 
-http.createServer(app).listen(8001, () => {
-  console.log("Express Server Start");
-});
+
+
+
+// http.createServer(app).listen(8001, () => {
+//   console.log("Express Server Start");
+// });
 
 app.use(
   "/home/hosting_users/bcd1031/apps/bcd1031_swc/images",
